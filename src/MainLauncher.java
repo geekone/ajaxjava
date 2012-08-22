248bee77e6f7499caf2c86347d01fc6b8f203936
geekone <ajaxj@qq.com>
2012-08-22 12:20:37 +0800

    init

---
+++ src/MainLauncher.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 12-8-22
+ * Time: 下午12:19
+ * To change this template use File | Settings | File Templates.
+ */
+public class MainLauncher {
+
+    public static void main(String[] args) {
+        System.out.println("启动");
+    }
+}
